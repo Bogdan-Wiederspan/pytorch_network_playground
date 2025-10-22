eee0e52248409ed0705f65580d81d3c319524ccb
Bogdan Wiederspan <b.wiederspan@web.de>
2025-10-22 15:30:21 +0200

    fixed slight wrong / in path

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
-export STORE_DIR=/data/dust/user/wiedersb/HH_DNN/
+export STORE_DIR=/data/dust/user/wiedersb/HH_DNN
 export SOFTWARE_DIR=${STORE_DIR}/software
 export TRAINING_DIR=${STORE_DIR}/training/results
-export CACHE_DIR=${STORE_DIR}/training/cache
+export CACHE_DIR=${STORE_DIR}/cache
 export INPUT_DATA_DIR="/data/dust/user/riegerma/hh2bbtautau/run3_training_data_prod14"
 export ML_ENV="ml_pytorch"
 export PYENV_ROOT="/afs/desy.de/user/w/wiedersb/.pyenv/"
