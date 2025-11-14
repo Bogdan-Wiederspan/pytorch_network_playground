986fe7368c31fd21d21b5dc3ea7da84a0bd984e5
Bogdan Wiederspan <b.wiederspan@web.de>
2025-11-14 12:17:31 +0100

    included weight normalization function to set all non linear layers decay factor to 0

--- src/train/optimizer.py
+++ src/train/optimizer.py
@@ -2,6 +2,68 @@ from __future__ import annotations
 
 import torch
 
+def normalized_weight_decay(
+    model: torch.nn.Module,
+    decay_factor: float = 1e-1,
+    normalize: bool = True,
+    apply_to: str = "weight",
+) -> tuple[dict, dict]:
+    """
+    Weight decay should only be applied to the linear layers or convolutional layers.
+    All other layers should not have weight decay applied.
+    Pytorch Optimizers will apply weight decay to all parameters that have `requires_grad=True`.
+    This function can be overwritten by specify the parameters that should have weight decay applied.
+
+    Args:
+        model (torch.nn.Module): The model to apply weight decay to.
+        decay_factor (float): The weight decay factor to apply to the parameters.
+        normalize (bool): If True, the decay factor is normalized by the number of parameters.
+            This ensures that the end L2 loss is about the same size for big and small models.
+
+    Returns:
+
+        tuple: A tuple containing two dictionaries where:
+            - The first dictionary contains parameters that should not have weight decay applied.
+            - The second dictionary contains parameters that should have weight decay applied.
+    """
+    # get list of parameters that should have weight decay applied, and those that should not
+    with_weight_decay = []
+    no_weight_decay = []
+    # only add weight decay to linear layers! everything else should run normally.
+    for module_name, module in model.named_modules():
+        # get only modules that are not containers
+        if len(list(module.named_modules())) == 1:
+            for parameter_name, parameter in module.named_parameters():
+                if (isinstance(module, torch.nn.Linear)) and (apply_to in parameter_name):
+                    with_weight_decay.append(parameter)
+                    print(f"add weight decay to: module:{module}//named: {module_name}// paramter:{parameter_name}")
+                else:
+                    no_weight_decay.append(parameter)
+
+    # decouple lambda choice from number of parameters, by normalizing the decay factor
+    num_weight_decay_params = sum([len(weight.flatten()) for weight in with_weight_decay])
+    if normalize:
+        decay_factor = decay_factor / num_weight_decay_params
+        print(f"Normalize weight decay by number of parameters: {decay_factor}")
+    return {"params": no_weight_decay, "weight_decay": 0.0}, {"params": with_weight_decay, "weight_decay": decay_factor}
+
+def prepare_weight_decay(model, optimizer_config) -> None:
+    # define which layers should contribute to the weight decay
+    no_weight_decay_parameters, weight_decay_parameters = normalized_weight_decay(
+        model,
+        decay_factor=optimizer_config["decay_factor"],
+        normalize=optimizer_config["normalize"],
+        # apply_to="weight.original0",
+        apply_to=optimizer_config["apply_to"],
+    )
+    return {"no_weight_decay_params": no_weight_decay_parameters, "weight_decay_params": weight_decay_parameters}
+
+def init_optimizer(optimizer, optimizer_config) -> None:
+    no_weight_decay_param, weight_decay_param = prepare_weight_decay(optimizer_config)
+    optimizer = optimizer(
+        (no_weight_decay_param, weight_decay_param),
+        lr=optimizer_config["learning_rate"],
+    )
 
 class SAM(torch.optim.Optimizer):
     """
