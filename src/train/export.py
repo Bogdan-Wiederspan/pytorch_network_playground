418b99961f165ac1b17e1932f12f31c7853813e5
Bogdan Wiederspan <b.wiederspan@web.de>
2025-11-24 16:37:57 +0100

    fix export with batch size 1

--- src/train/export.py
+++ src/train/export.py
@@ -7,15 +7,24 @@ def torch_export(model, dst_path, input_tensors):
     model = model.eval()
 
     categorical_input, continuous_inputs = input_tensors
+
     continuous_inputs = continuous_inputs.to(torch.float32)
     categorical_input = categorical_input.to(torch.int32)
+
+    # batch size 1 is an edge case and overwrites dynamic batch size, thus, inflate input to prevent this
+    if categorical_input.shape[0] == 1:
+        continuous_inputs = torch.concatenate((continuous_inputs, continuous_inputs))
+        categorical_input = torch.concatenate((categorical_input, categorical_input))
+
     # HINT: input is chosen since model takes a tuple of inputs, normally name of inputs is used
     # dim = torch.export.dynamic_shapes.Dim.AUTO
-    dim = torch.export.dynamic_shapes.Dim.AUTO
+    dim = torch.export.Dim("batch")
 
     dynamic_shapes = {
-        "categorical_inputs": (dim, categorical_input.shape[-1]),
-        "continuous_inputs" : (dim, continuous_inputs.shape[-1]),
+        # "categorical_inputs": (dim, categorical_input.shape[-1]),
+        # "continuous_inputs" : (dim, continuous_inputs.shape[-1]),
+        "categorical_inputs": {0:dim, 1:categorical_input.shape[-1]},
+        "continuous_inputs" : {0:dim, 1:continuous_inputs.shape[-1]},
     }
 
     exp = torch.export.export(
