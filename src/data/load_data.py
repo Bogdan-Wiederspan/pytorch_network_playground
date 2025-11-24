- Fixed bug in k-fold splitting, where by accident indices usage was wrong - added wrapper for model to add softmax at end of model, necessary for export to cf - more adjustments to be compareable to marcels dnn --> resulting in perfect agreement - streamlined config - added dnn_debug.py script to compare to marcels dnn

src/data/load_data.py
@@ -13,24 +13,25 @@ from data.cache import DataCacher
 
 logger = get_logger(__name__)
 
-
-def find_datasets(dataset_patterns: str, year_patterns: str, file_type: str="root"):
+def find_datasets(dataset_patterns: list[str], year_patterns: list[str], file_type: str="root", verbose=True):
     """
-    Find all files in ${INPUT_DATA_DIR} by pattern defined by <year_pattern>/<dataset_pattern>/*.file_type and
+    Find all files in ${INPUT_DATA_DIR} by glob pattern defined by <year_pattern>/<dataset_pattern>/*.file_type and
     return the result as a dictionary with dataset names as keys and list of file paths as values.
+    Duplicates in *year_pattern* and *dataset_patterns* DO NOT change result.
 
     Args:
-        dataset_patter (str): _description_
-        year_pattern (str): Pattern describing the years
+        dataset_patterns (list[str]): Pattern describing our nano AODs, ex: ['hh_ggf_hbb_htt_kl*_kt1*']
+        year_pattern (str): Pattern describing the years.
         file_type (str, optional): File extension . Defaults to "root".
 
     Returns:
         dict: dictionary with dataset names as keys and list of file paths as values
     """
+    # get data dir from config
     if (data_dir := os.environ.get("INPUT_DATA_DIR", None)) is None:
         raise ValueError("Environment variable INPUT_DATA_DIR not set! Source setup.sh")
 
-    # wrapp patterns in lists if only being a single string
+    # wrapp patterns since iterable is assumed
     if isinstance(dataset_patterns, str):
         dataset_patterns = [dataset_patterns]
     if isinstance(year_patterns, str):
@@ -38,9 +39,11 @@ def find_datasets(dataset_patterns: str, year_patterns: str, file_type: str="roo
 
     file_pattern = f"*.{file_type}"
 
+    # resolve year pattern and remove duplicates
     years = []
     for year_pattern in year_patterns:
         years += [year.name for year in pathlib.Path(data_dir).glob(f"{year_pattern}")]
+    years = sorted(set(years))
 
     data = {}
     for year in years:
@@ -55,8 +58,10 @@ def find_datasets(dataset_patterns: str, year_patterns: str, file_type: str="roo
                 files = list(map(str, pathlib.Path(dataset).glob(file_pattern)))
                 if len(files) == 0:
                     raise ValueError(f"{dataset.name} has 0 files")
-                size = round(sum(os.path.getsize(f) for f in files) / (1024**2),2)
-                logger.info(f"+{len(files)} files | size {size} MB | {year}/{dataset.name}")
+
+                if verbose:
+                    size = round(sum(os.path.getsize(f) for f in files) / (1024**2),2)
+                    logger.info(f"+{len(files)} files | size {size} MB | {year}/{dataset.name}")
                 data[year][dataset.name] = files
     if not data:
         raise ValueError("No datasets found with given patterns")
@@ -87,13 +92,16 @@ def root_to_numpy(files_path: Union[list[str],str], branches: Union[list[str], s
     if depthCount(branches) > 1 and branches is not None:
         raise ValueError("branches must be a flat list")
 
-    # include meta fields that are necessary for every training
-    # process_id is for filtering after subprocesses
-    # normalizaton_weight is for
-    meta_fields = {"process_id", "normalization_weight", "tau2_isolated", "leptons_os", "channel_id"}
+    # include meta fields that are necessary for preprocessing and baseline cuts
+    # fields -> purpose:
+    #   process_id -> filtering of subphasespaces
+    #   normalization_weight -> used as sample weight for batch
+    #   tau2_isolated, lepton_os, channel_id -> used for baseline cut
+    #   event -> event number used for k-fold splitting
+    meta_fields = {"process_id", "normalization_weight", "tau2_isolated", "leptons_os", "channel_id", "event"}
     branches = set(branches).union(meta_fields)
 
-    # handle cuts
+    # handle baseline combine with additional cuts
     baseline_cuts = [
         "(tau2_isolated == 1)",
         "(leptons_os == 1)",
@@ -106,12 +114,12 @@ def root_to_numpy(files_path: Union[list[str],str], branches: Union[list[str], s
         cut = []
     cuts = baseline_cuts + cut
 
-
+    # load root files and combine the array to continogus arrays
     if isinstance(files_path, str):
         files_path = [files_path]
     arrays = []
-    logger.info(f"loading: {pathlib.Path(files_path[0]).parent}")
     for file_path in files_path:
+        logger.info(f"loading: {file_path}")
         with uproot.open(file_path, object_cache=None, array_cache=None) as file:
             tree = file["events"]
             arrays.append(tree.arrays(branches, library="ak", cut="&".join(cuts)).to_numpy())
@@ -138,7 +146,7 @@ def parquet_to_awkward(files_path: Union[list[str],str], columns: Union[list[str
 
 def get_loader(file_type: str, **kwargs):
     """
-    Small helper function to get the correct loader function and its configuration based on the file type.
+    Small helper function to load correct loader function and its configuration based on given *file_type*.
 
     Args:
         file_type (str): file extension.
@@ -284,9 +292,13 @@ def convert_numpy_to_torch(events, continous_features, categorical_features, dty
             ]
         # convert to torch
         weight = torch.tensor(np.sum(arr["normalization_weight"]), dtype = dtype)
+        # event id
+        event_id = torch.tensor(np.ascontiguousarray(arr["event"]), dtype=torch.uint64)
+
         events[uid] = {
             "continous": continous_tensor,
             "categorical": categorical_tensor,
-            "weight": weight
+            "weight": weight,
+            "event_id" : event_id,
         }
     return events

src/data/preprocessing.py
@@ -61,7 +61,7 @@ def get_batch_statistics_from_sampler(sampler, padding_values=None, features=Non
     logger.info("Calculate mean and std over all subphase spaces")
     # filter keys after processes
     weighted_means = []
-    weighted_stds = []
+    weighted_vars = []
     features_dict = sampler.get_attribute_of_datasets("continous_input")
     weights_dict = sampler.get_attribute_of_datasets("relative_weight")
     sum_of_weights = sum(list(weights_dict.values()))
@@ -70,7 +70,7 @@ def get_batch_statistics_from_sampler(sampler, padding_values=None, features=Non
     # do weighted mean,std over all uid with weight
     for current_pid_idx, pid in enumerate(features_dict.keys(), start = 1):
         # go throught each feature axis and calculate statitic per feature
-        f_means, f_stds = [], []
+        f_means, f_vars = [], []
         array = features_dict[pid]
         num_f = array.shape[-1]
         print(f"calculating stats for {pid} status:{current_pid_idx}/{len(features_dict)}\t\t\r",end=" ", flush=True)
@@ -90,30 +90,30 @@ def get_batch_statistics_from_sampler(sampler, padding_values=None, features=Non
             masked_array = feature_array[~padding_mask]
 
             masked_mean = masked_array.mean(axis=0)
-            masked_std = masked_array.std(axis=0)
+            masked_var = masked_array.var(axis=0)
             # if anything got a nan go into debug mode to investigate
             if torch.isnan(masked_mean):
                 from IPython import embed; embed(header=f"{pid} is nan check feature_array and sampler")
 
             f_means.append(masked_mean)
-            f_stds.append(masked_std)
+            f_vars.append(masked_var)
 
         # weight mean and add to collection
         pid_weight = weights_dict[pid]
         weighted_means.append(torch.tensor(f_means) * pid_weight)
-        weighted_stds.append(torch.tensor(f_stds) * pid_weight)
+        weighted_vars.append(torch.tensor(f_vars) * pid_weight)
 
-    # calculate weighted average over uid means and std
+    # calculate weighted average over uid means and var
     w_avg_mean  = torch.sum(torch.stack(weighted_means, axis=0), axis = 0) / sum_of_weights
-    w_avg_std = torch.sum(torch.stack(weighted_stds, axis=0), axis = 0) / sum_of_weights
+    w_avg_var = torch.sum(torch.stack(weighted_vars, axis=0), axis = 0) / sum_of_weights
     if features:
         print("Statistics")
-        for f_name, f_mean, f_std in zip(features, w_avg_mean, w_avg_std):
-            print(f"{f_name:<30}: mean:{f_mean:>10.4} std:{f_std:>10.4}")
-    return w_avg_mean, w_avg_std
+        for f_name, f_mean, f_var in zip(features, w_avg_mean, w_avg_var):
+            print(f"{f_name:<30}: mean:{f_mean:>10.4} var:{f_var:>10.4}")
+    return w_avg_mean, w_avg_var.sqrt()
 
 
-def get_batch_statistics(events, padding_value=0):
+def get_batch_statistics(events=None, padding_value=0):
     """
     Calculates the weighted mean and standard deviation over all subphase spaces of a process in *events*.
     The data is expected to be of form : {"unique_identifier_tuple": {continous: arr}, {weight}: arr}.
@@ -211,7 +211,7 @@ def get_batch_statistics_per_dataset(events, padding_value=0):
         stats[process_type] = nom / denom
     return stats
 
-def k_fold_indices(num_events, c_fold, k_fold, seed, test=False):
+def k_fold_indices(event_id, c_fold, k_fold, seed, test=False):
     """
     Creates idicies for training and validation from *k_fold*, where *c_fold* is the test fold.
     The indicies are permutated using *seed*. If *test* is True, the test set is returned if False the k-1 folds are returned.
@@ -222,22 +222,21 @@ def k_fold_indices(num_events, c_fold, k_fold, seed, test=False):
         k_fold (int): Number of folds
         seed (int): Seeds used for random permutation
     """
-    indices = torch.arange(num_events)
-
     # true => test folds, false => train and validation folds
     # if no kfold is wished than set test to 0 and return everyrthing
     if k_fold == 0:
-        raise(f"Can't do k-fold with desired k_fold of {k_fold}, needs to be > 0")
-    else:
-        test_fold_mask = indices % k_fold == c_fold
+        raise ValueError(f"Can't do k-fold with desired k_fold of {k_fold}, needs to be > 0")
+
+    test_fold_mask = event_id % k_fold == c_fold
+    indices = torch.arange(len(event_id))
 
     if test:
-        sub_indices = indices[test_fold_mask]
+        sub_event_id = indices[test_fold_mask]
     else:
-        sub_indices = indices[~test_fold_mask]
+        sub_event_id = indices[~test_fold_mask]
     # apply mask and randomize according to given seed
-    sub_indices = torch.randperm(len(sub_indices), generator=torch.Generator().manual_seed(seed))
-    return sub_indices[sub_indices]
+    randomized = torch.randperm(len(sub_event_id), generator=torch.Generator().manual_seed(seed))
+    return sub_event_id[randomized]
 
 def split_array_to_train_and_validation(array, trainings_proportion=0.75):
     """
@@ -252,13 +251,13 @@ def split_array_to_train_and_validation(array, trainings_proportion=0.75):
     """
     if (trainings_proportion > 1) or (trainings_proportion < 0):
         raise ValueError(f"Split fraction is {trainings_proportion} but needs to be in range of 0 and 1")
-    train_length = round((len(array) * trainings_proportion))
+    train_length = int(round((len(array) * trainings_proportion)))
     t_idx = array[:train_length]
     v_idx = array[train_length:]
     return t_idx, v_idx
 
 
-def split_k_fold_into_training_and_validation(events_dict, c_fold, k_fold, seed, train_ratio=0.75):
+def split_k_fold_into_training_and_validation(events_dict, c_fold, k_fold, seed, train_ratio=0.75, return_test=False):
     """
     Takes *events_dict* where continous and categorical data and split these into *k_fold* where *c_fold* is the holdout test seed.
     A random permutation happens using *seed* and in the end the k-1 folds are split into training
@@ -279,19 +278,16 @@ def split_k_fold_into_training_and_validation(events_dict, c_fold, k_fold, seed,
         array = events_dict.pop(uid)
         train[uid] = {"weight" : array["weight"]}
         valid[uid] = {"weight" : array["weight"]}
+        tv_indices = k_fold_indices(array["event_id"], c_fold, k_fold, seed, test=return_test)
+        t_idx, v_idx = split_array_to_train_and_validation(tv_indices, train_ratio)
 
-        for key in ("continous", "categorical"):
+        for key in ("continous", "categorical", "event_id"):
             arr = array.pop(key)
-            num_events = len(arr)
-            tv_indices = k_fold_indices(num_events, c_fold, k_fold, seed, test=False)
-            t_idx, v_idx = split_array_to_train_and_validation(tv_indices, train_ratio)
-
             t_arr = arr[t_idx]
             v_arr = arr[v_idx]
 
             train[uid][key] = t_arr
             valid[uid][key] = v_arr
-
     # edge case split results in empty tensors (due to very low event count) remove these
     # if empty do not save
     for uid in list(train.keys()):

src/models/create_model.py
@@ -61,7 +61,8 @@ class BNet(torch.nn.Module):
 
     def init_layers(self, continous_features, categorical_features, config):
         # increasing eps helps to stabilize training to counter batch norm and L2 reg counterplay when used together.
-        eps = 0.5e-5
+        # eps = 0.5e-5
+        eps = 0.001
         # activate weight normalization on linear layer weights
         normalize = False
 
@@ -112,13 +113,19 @@ class BNetDenseNet(torch.nn.Module):
 
     def init_layers(self, continous_features, categorical_features, config):
         # increasing eps helps to stabilize training to counter batch norm and L2 reg counterplay when used together.
-        eps = 0.5e-5
+        eps = 0.001
         # activate weight normalization on linear layer weights
         normalize = False
 
         # helper where all layers are defined
         # std layers are filled when statitics are known
-        self.std_layer = StandardizeLayer(mean=config["mean"], std=config["std"])
+        if config["mean"] is None and config["std"] is None:
+            self.std_layer = StandardizeLayer(
+                mean = torch.zeros(len(continous_features)),
+                std = torch.ones(len(continous_features))
+            )
+        else:
+            self.std_layer = StandardizeLayer(mean=config["mean"], std=config["std"])
 
         self.continuous_padding = PaddingLayer(padding_value=-4, mask_value=EMPTY_FLOAT)
 
@@ -149,7 +156,7 @@ class BNetDenseNet(torch.nn.Module):
             "skip_connection_init":config["skip_connection_init"],
             "freeze_skip_connection":config["freeze_skip_connection"],
             "activation_functions":"ELU",
-            "eps":1e-5,
+            "eps":eps,
             "normalize":False,
         }
 
@@ -170,4 +177,22 @@ class BNetDenseNet(torch.nn.Module):
         x = self.dense_block_4(x)
         x = self.dense_block_5(x)
         x = self.last_linear(x)
+        return x
+
+class AddActFnToModel(torch.nn.Module):
+    def __init__(self, model, act_fn, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model
+        self.act_func = self._get_attr(torch.nn.modules.activation, act_fn)(dim=1)
+
+    def _get_attr(self, obj, attr):
+        for o in dir(obj):
+            if o.lower() == attr.lower():
+                return getattr(obj, o)
+        else:
+            raise AttributeError(f"Object has no attribute '{attr}'")
+
+    def forward(self, categorical_inputs, continuous_inputs):
+        x = self.model(categorical_inputs, continuous_inputs)
+        x = self.act_func(x)
         return x

src/models/layers.py
@@ -388,6 +388,7 @@ class InputLayer(torch.nn.Module):  # noqa: F811
         return x
 
     def forward(self, categorical_inputs, continuous_inputs):
+        # HINT: When comparing this layer with other compare order of inputs
         x = torch.cat(
             [
                 self.continous_preprocessing_pipeline(continuous_inputs),
@@ -428,7 +429,7 @@ class DenseNetBlock(torch.nn.Module):
     def forward(self, x):
         _input = x * self.skip_connection_amplifier
         x = self.dense_block(x)
-        x = torch.concatenate((_input, x), dim = 1)
+        x = torch.concatenate((x, _input), dim = 1)
         return x
 
 class ResNetBlock(torch.nn.Module):  # noqa: F811

src/train/export.py
@@ -2,19 +2,25 @@ import torch
 
 def torch_export(model, dst_path, input_tensors):
     from pathlib import Path
+    from models.create_model import AddActFnToModel
+    model = AddActFnToModel(model, "softmax")
     model = model.eval()
 
-    categorical_input, continous_inputs = input_tensors
-
+    categorical_input, continuous_inputs = input_tensors
+    continuous_inputs = continuous_inputs.to(torch.float32)
+    categorical_input = categorical_input.to(torch.int32)
     # HINT: input is chosen since model takes a tuple of inputs, normally name of inputs is used
+    # dim = torch.export.dynamic_shapes.Dim.AUTO
     dim = torch.export.dynamic_shapes.Dim.AUTO
+
     dynamic_shapes = {
-        "input": ((dim, categorical_input.shape[-1]), (dim, continous_inputs.shape[-1]))
+        "categorical_inputs": (dim, categorical_input.shape[-1]),
+        "continuous_inputs" : (dim, continuous_inputs.shape[-1]),
     }
 
     exp = torch.export.export(
         model,
-        args=((categorical_input, continous_inputs),),
+        args=(categorical_input, continuous_inputs),
         dynamic_shapes=dynamic_shapes,
     )
 

src/train/train.py
@@ -15,13 +15,14 @@ from train_config import (
 )
 from train_utils import training, validation, log_metrics
 
+logger = get_logger(__name__)
+
 CPU = torch.device("cpu")
 CUDA = torch.device("cuda")
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 VERBOSE = False
 
-logger = get_logger(__name__)
-
+logger.info(f"Running DEVICE is {DEVICE}")
 # load data
 tboard_writer = TensorboardLogger(name=hash_config(config))
 logger.warning(f"Tensorboard logs are stored in {tboard_writer.path}")
@@ -34,14 +35,16 @@ for current_fold in (config["train_folds"]):
 
     # load data from cache is necessary or from root files
     # events is of form : {uid : {"continous","categorical", "weight": torch tensor}
-    events = get_data(dataset_config, overwrite=False, _save_cache=True)
+    # events = get_data(dataset_config, overwrite=False, _save_cache=True)
     # create k-folds, whe current fold is test fold and leave out
+    events = get_data(dataset_config, overwrite=False, _save_cache=True)
+
     train_data, validation_data = split_k_fold_into_training_and_validation(
         events,
         c_fold=current_fold,
         k_fold=config["k_fold"],
         seed=config["seed"],
-        train_ratio=config["train_ratio"],
+        train_ratio=0.75,
     )
 
     training_sampler = create_train_or_validation_sampler(
@@ -65,6 +68,7 @@ for current_fold in (config["train_folds"]):
     training_sampler.share_weights_between_sampler(validation_sampler)
 
     # get weighted mean and std of expected batch composition
+
     model_building_config["mean"], model_building_config["std"] = get_batch_statistics_from_sampler(
         training_sampler,
         padding_values=-99999,
@@ -74,6 +78,11 @@ for current_fold in (config["train_folds"]):
     ### Model setup
     model = create_model.BNetDenseNet(dataset_config["continous_features"], dataset_config["categorical_features"], config=model_building_config)
     model = model.to(DEVICE)
+
+    # ## load mean from marcel
+    # import marcel_weight_translation as mwt
+    # model = mwt.load_marcels_weights(model, continous_features=dataset_config["continous_features"])
+
 
     # TODO: only linear models should contribute to weight decay
     # TODO : SAMW Optimizer