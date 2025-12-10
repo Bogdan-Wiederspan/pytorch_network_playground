653236b52dd4a952b19e2d33b9b7d47cdf348a2c
Bogdan Wiederspan <b.wiederspan@web.de>
2025-12-10 14:13:41 +0100

    moved embedding expected inputs to features.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -65,19 +65,6 @@ def normalized_weight_decay(
         print(f"Normalize weight decay by number of parameters: {decay_factor}")
     return {"params": no_weight_decay, "weight_decay": 0.0}, {"params": with_weight_decay, "weight_decay": decay_factor}
 
-
-embedding_expected_inputs = {
-    "res_dnn_pnet_pair_type": [0, 1, 2],  # see mapping below
-    "res_dnn_pnet_dm1": [-1, 0, 1, 10, 11],  # -1 for e/mu
-    "res_dnn_pnet_dm2": [0, 1, 10, 11],
-    "res_dnn_pnet_vis_tau1_charge": [-1, 1],
-    "res_dnn_pnet_vis_tau2_charge": [-1, 1],
-    "res_dnn_pnet_has_fatjet": [0, 1],  # whether a selected fatjet is present
-    "res_dnn_pnet_has_jet_pair": [0, 1],  # whether two or more jets are present
-    # 0: 2016APV, 1: 2016, 2: 2017, 3: 2018, 4: 2022preEE, 5: 2022postEE, 6: 2023pre, 7: 2023post
-    "res_dnn_pnet_year_flag": [0, 1, 2, 3, 4, 5, 6, 7],
-    "channel_id": [1, 2, 3],
-}
 # embedding_expected_inputs = {
 #     "pair_type": [0, 1, 2],  # see mapping below
 #     "decay_mode1": [-1, 0, 1, 10, 11],  # -1 for e/mu
