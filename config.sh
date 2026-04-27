1f7df805b36b9d1b50dfd8cf79ac486b7c788c08
Bogdan Wiederspan <b.wiederspan@web.de>
2026-04-27 14:38:26 +0200

    include new mode to switch between pyenv and cf_sandbox usage. Useful for bachelor students when using this setup

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 # storage directories
-export STORE_DIR=/data/dust/user/wiedersb/HH_DNN
-export CACHE_DIR=${STORE_DIR}/cache
+export STORE_DIR=/data/dust/user/wiedersb/HH_DNN # ROOT of storage
+export CACHE_DIR=${STORE_DIR}/cache # directory where preprocessed data is stored
 export PICTURE_DIR=${STORE_DIR}/pictures/features
-export MODELS_DIR=${STORE_DIR}/models # saved directories
+export MODELS_DIR=${STORE_DIR}/models # saved models
 export TENSORBOARD_DIR=${STORE_DIR}/tensorboard # tensorboard storage
 
 # debug level: DEBUG; INFO; WARNING
@@ -13,6 +13,8 @@ export FILE_LOG_LEVEL="DEBUG"
 export INPUT_DATA_DIR="/data/dust/user/riegerma/hh2bbtautau/run3_training_data/prod20"
 #export INPUT_DATA_DIR="/data/dust/user/riegerma/hh2bbtautau/run3_training_data/prod14"
 
-# pyenv handling: not necessary when own environment is used
-export ML_ENV="ml_pytorch" # venv name
-export PYENV_ROOT="/afs/desy.de/user/w/wiedersb/.pyenv/" # pyenv root
+# virtualenv handling
+export VENV_MODE="pyenv" # pyenv or cf, handles switch
+export ML_ENV="ml_pytorch" # venv name of virtualenv
+export CF_SANDBOX="venv_hbt_dev" #  sandbox name in columnflow
+export PYENV_ROOT="/afs/desy.de/user/w/wiedersb/.pyenv/" # root of pyenv installation
--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 
 pyenv_activate ()
 {
-    export PYENV_ROOT="/afs/desy.de/user/w/wiedersb/.pyenv/";
+    # bootstrap pyenv to be activate for current bash session
+    echo "Bootstrap Pyenv"
     export PATH="${PYENV_ROOT}/bin/:${PATH}";
     eval "$(pyenv init --path)";
     eval "$(pyenv init -)";
@@ -11,37 +12,80 @@ pyenv_activate ()
 }
 
 setup_env() {
-    # Prevent double execution
+    # Setup Virtualenv and mount given environment in config
+
+    # check if inside virtualenv -> if inside sys prefix changes
+    # Source - https://stackoverflow.com/a/15454916
+    local IN_VENV=$(python -c 'import sys; print ("1" if hasattr(sys, "real_prefix") else "0")')
+    # -n is true if NON-ZERO, -z true if EMPTY
+    if [ "${IN_VENV}" = "1" ]; then
+        echo "Already inside virtualenv"
+        return 4
+    fi
+
+    # activate pyenv or cf setup
+    if [ "${VENV_MODE}" = "pyenv" ]; then
+        if [ -n "${PYENV_ROOT}" ] && [ -n "${ML_ENV}" ]; then
+            # check if pyenv exists as command, if not activate it
+            # command return 0 if exist, else another number
+            # >/dev etc. just silence the output
+            if ! command -v pyenv >/dev/null 2>&1; then
+                pyenv_activate
+            pyenv activate "${ML_ENV}"
+            echo "activate virtualenv ${ML_ENV}"
+            fi
+        else
+            echo "can' activate pyenv due too wrong PYEV_ROOT or not set ML_ENV"
+            return 1
+        fi
+    # for bachelor and master students that use columnflow sandboxes
+    elif [ "${VENV_MODE}" = "cf" ]; then
+        echo "Sourcing cf sandbox"
+
+        if [ -n "${CF_SANDBOX}" ]; then
+            if command -v cf_sandbox >/dev/null 2>&1; then
+                cf_sandbox ${CF_SANDBOX}
+            else
+                echo "Source your columnflow setup!"
+                return 2
+            fi
+        else
+            echo "CF_SANDBOX is not a valid columnflow sandbox - check if sandbox exist"
+            return 2
+        fi
+
+    else
+        echo "VENV_MODE can only be cf or pyenv, but got ${VENV_MODE}"
+        return 3
+    fi
+    # extend PS1 if environment is set properly
+    # Show only the last part of the virtualenv path (env name) in the prompt
+    export PS1="[${VIRTUAL_ENV##*/}] ${PS1}"
+    # include source of project as root for python
+    export PYTHONPATH=$"(pwd)/src"
+    return 0
+}
+
+setup() {
+    # Prevent double execution, when SETUP_COMPLETE is set (at end of script) don't run setup twice.
     if [ -n "$SETUP_COMPLETE" ]; then
         echo "Setup already completed. Skipping."
         return 0
     fi
-    # Get the directory where this script is located
-    export LOCAL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-    # Source config.sh from the same directory
+    # Get the directory where this script is located and source config located there
+    # BASH_SOURCE[0] = path of current script, get dir and resolve absolute path
+    local LOCAL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
     source "$LOCAL_DIR/config.sh"
+    setup_env
 
-    # Activate pyenv environment stored in ML_ENV
-    if [ -n "$ML_ENV" ]; then
-        # check if pyenv exists if not source before
-        pyenv activate "$ML_ENV"
-        echo "activate virtualenv ${ML_ENV}"
-        export PS1="[${VIRTUAL_ENV##*/}] $PS1"
-
-    else
-        echo "ML_ENV is not set."
-        return 1
+    if [ "${SETUP_DONE}" = 0 ]; then
+        echo "Ready to go"
+        #export SETUP_COMPLETE=1
     fi
-    # Show only the last part of the virtualenv path (env name) in the prompt
-    export PYTHONPATH=$(pwd)/src
-    echo "Ready to go"
-    export SETUP_COMPLETE=1
+
+    return 0
 }
 
-# entry point
-if ! command -v pyenv >/dev/null 2>&1; then
-    pyenv_activate
-fi
 
-setup_env "$@"
+setup "$@"
