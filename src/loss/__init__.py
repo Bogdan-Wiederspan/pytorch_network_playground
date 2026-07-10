ca3a256dfc40f2409607a2f150d65022772a3597
Bogdan Wiederspan <b.wiederspan@web.de>
2026-07-10 14:40:10 +0200

    Separate BinningAware Significance and Significance Loss into separate files

--- src/loss/__init__.py
+++ src/loss/__init__.py
@@ -1,11 +1,21 @@
 from utils.lazy_loader import lazy_import
 
 from . import binning
-from .SignalEfficiency import BinningAwareSignificance, SignalEfficiency
+from .SignalEfficiency import SignalEfficiency
+from .BinningAwareSignalEfficiency import BinningAwareSignificance
 from .CrossEntropy import WeightedCrossEntropy
 from .FocalLoss import FocalLoss
 from .utils import init_loss
 
+
+__all__ = [
+    "binning",
+    "SignalEfficiency",
+    "BinningAwareSignificance",
+    "WeightedCrossEntropy",
+    "FocalLoss",
+    "init_loss",
+]
 
 def __getattr__(name):
     return lazy_import(__name__, globals(), name)
